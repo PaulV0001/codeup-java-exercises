9445519017deedbdafae1a5ca3c9286c59482454
paulvaldez <valdezpaul00@gmail.com>
2017-09-18 15:22:01 -0500

    Solutions for first exercise.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,26 @@ public class HelloWorld {
 //    psvm
     public static void main(String[] args) {
 //        sout
+        byte byteNumber = 2;
+        short shortNumber = 0;
+        int intNumber = 1;
+        long longNumber = 10;
+
+//        floating point numbers
+        float floatNumber = 2.0f;
+        double doubleNumber = 10.5;
+
+        char singCharacter = 'L';
+
+        boolean booleanValue = true;
+
+        String name = "Redwood";
+
+//        const MY_CONSTANT = 4;
+        final int MY_CONSTANT = 4;
+
+        System.out.println(3 + 4);
+
         System.out.println("Hello World!");
     }
 }
---
+++ src/JavaSyntax.java
@@ -0,0 +1,33 @@
+public class JavaSyntax {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 13;
+
+        double myString = 3.14159;
+
+        float myNumber = 3.14f;
+
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        can only be one int with x
+
+
+//        int x = 4;
+//        System.out.println(x + 5);
+
+//        int x = 3;
+//        int y = 4;
+//        System.out.println(x * y);
+
+        int x = 10;
+        int y = 2;
+        System.out.println(x / y);
+        System.out.println(y - x);
+
+        System.out.println(myFavoriteNumber);
+        System.out.println(myString);
+        System.out.println(myNumber);
+    }
+}
