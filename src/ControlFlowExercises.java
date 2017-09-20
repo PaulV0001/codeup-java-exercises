c798cbe4841d94281fabc8d85fc8bc25e447cd59
paulvaldez <valdezpaul00@gmail.com>
2017-09-20 09:08:20 -0500

    Completed the third exercise of the loops exercises.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -49,6 +49,47 @@ public class ControlFlowExercises {
             }
         }
 
+        System.out.println("Welcome to the Squares and Cubes table");
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+
+        do
+        {
+            // Get input from the user
+            System.out.print("What would you like to go up to? ");
+            int integer = sc.nextInt();
+
+            // Create a header
+            String header = "Number  " + "Squared " + "Cubed   " + "\n"
+                    +   "======  " + "======= " + "=====   ";
+            System.out.println(header);
+
+            int square = 0;
+            int cube = 0;
+
+            String row = "";
+            for (int i = 1; i <= integer; i++)
+            {
+
+                square = i * i;
+                cube = i * i * i;
+
+                row = i + "       " + square + "       " + cube;
+                System.out.println(row);
+            }
+
+            // See if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+
         }
+        while (!choice.equalsIgnoreCase("n"));
     }
+
+
+        }
+
 
