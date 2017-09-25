1bdead35468d44f3ab9defd8a0f7e5ceb53cab2a
paulvaldez <valdezpaul00@gmail.com>
2017-09-25 13:41:18 -0500

    Completed the first exercise in the Person exercises.

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+public class Person {
+    private String name;
+
+    // returns the person's name
+    public String getName;
+
+
+    public Person(String aGetName) {
+        getName = aGetName;
+    }
+
+    public Person() {
+
+    }
+
+    public String name() {
+        return getName;
+    }
+
+}
---
+++ src/PersonClass.java
@@ -0,0 +1,9 @@
+public class PersonClass {
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.getName = "Paul";
+
+        System.out.println(person.getName);
+    }
+}
