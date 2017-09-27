73af768b67c86e188ff2369c88aad170793a6c96
paulvaldez <valdezpaul00@gmail.com>
2017-09-27 10:33:37 -0500

    Added the different files for the second exercise in the Arrays exercise.

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,25 @@
+package movies;
+import java.util.
+
+
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+        Input input = new Input();
+
+        System.out.println("What would you like to do?\n" +
+                "\n" +
+                "0 - exit\n" +
+                "1 - view all movies\n" +
+                "2 - view movies in the animated category\n" +
+                "3 - view movies in the drama category\n" +
+                "4 - view movies in the horror category\n" +
+                "5 - view movies in the scifi category\n");
+
+
+
+
+
+//        System.out.println(Arrays.toString(MoviesArray.findAll()));
+    }
+}
